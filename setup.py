Kitware/SMQTK-Relevancy

commit 82f3e4bce8f8ee2f7e19ae4578f698bbf3b87ce8

Added optional use of file-magic module for DataFileElement
If importable, try to use file-magic mime-type identification from file
bytes first. This should be more performant that use of tika and is more
robust than just checking file name extension string.

setup.py
@@ -154,6 +154,9 @@ setuptools.setup(
         'flann': [
             'pyflann>=1.8.4',
         ],
+        'libmagic', [
+            'file-magic',
+        ],
         'postgres': [
             'psycopg2',
         ],

commit a6bd6fd277eb8fb3a23ca9299814d4e6c8dfde3a

Fix syntax error in setup.py introduced by 82f3e4b

setup.py
@@ -154,7 +154,7 @@ setuptools.setup(
         'flann': [
             'pyflann>=1.8.4',
         ],
-        'libmagic', [
+        'libmagic': [
             'file-magic',
         ],
         'postgres': [