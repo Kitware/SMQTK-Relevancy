f9a0d26b91aaaf7eb0b84c63b1f6b2b6b047c050
andrew.matrai <andrew.matrai@kitware.com>
2021-05-20 14:12:46 -0400

    remove setuptools and pkg_resources deps
    corrects
    
    fix
    
    fix
    
    update
    
    update versions

--- smqtk_relevancy/__init__.py
+++ smqtk_relevancy/__init__.py
@@ -1,4 +1,2 @@
-import pkg_resources
-
-from .interfaces.rank_relevancy import RankRelevancy, RankRelevancyWithFeedback  # noqa: F401
-__version__ = pkg_resources.get_distribution(__name__).version
+from .interfaces.rank_relevancy import \
+    RankRelevancy, RankRelevancyWithFeedback  # noqa: F401
